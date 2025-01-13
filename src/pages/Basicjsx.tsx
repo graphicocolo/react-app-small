29dbb8fbd0fd1a68f6125d52aa4e0d2b0e2a6576
高橋礼 <graphicocolo@gmail.com>
2025-01-13 13:07:10 +0900

    React 編:Stateの管理

--- src/pages/Basicjsx.tsx
+++ src/pages/Basicjsx.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Navigation from '@/components/Navigation';
 import Greeting from '@/components/Greeting';
 import LoggedIn from '@/components/LoggedIn';
@@ -13,6 +15,12 @@ const Basicjsx = () => {
   const notDoneGreeting = 'not yet.';
   const isLoggedIn = false;
   const isDone = true;
+
+  const [inputValue, setInputValue] = useState<string>('');
+  const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+  };
   return (
     <>
       <Navigation />
@@ -37,6 +45,12 @@ const Basicjsx = () => {
         ) : (
           <p className="text-red-500">{notDoneGreeting}</p>
         )}
+      </div>
+      <div className="flex flex-col items-center mt-32">
+        <form>
+          <input type="text" value={inputValue} onChange={handleInputChange} className="border border-slate-300 rounded" />
+        </form>
+        <p>ここに入力欄のテキストが反映される<span className="pl-2 text-blue-600 font-bold">{inputValue}</span></p>
       </div>
     </>
   )
